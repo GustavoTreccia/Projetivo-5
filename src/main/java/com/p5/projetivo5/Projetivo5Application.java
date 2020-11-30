450c0a5a47802603c26979b6bc6cb44edbffa917
GustavoTreccia <g.treccia@outlook.com>
2020-11-30 09:34:46 -0300

    Backup 1

---
+++ src/main/java/com/p5/projetivo5/Projetivo5Application.java
@@ -0,0 +1,13 @@
+package com.p5.projetivo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetivo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetivo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/p5/projetivo5/Projetivo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p5.projetivo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetivo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
